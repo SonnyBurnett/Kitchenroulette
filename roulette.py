3d904ab8a6f2235d16bd245338c8fbb63411c30f
Taco Bakker <tsbakker65@gmail.com>
2025-12-24 12:22:08 +0100

    Check the code for possible errors

--- roulette.py
+++ roulette.py
@@ -6,7 +6,9 @@ from datetime import datetime
 
 
 #####################################################
-
+# De lijst met kokers en eters wordt in een willekeurige volgorde gezet.
+# Zodat er automatisch een ander schema uit gaat komen.
+#
 
 def maak_een_indeling(huidige_gang, huizen):
     kook_ploeg = [huis for huis in huizen if huis.get_gang() == huidige_gang]
@@ -18,6 +20,12 @@ def maak_een_indeling(huidige_gang, huizen):
     print("Eters:", [eet.get_adres() for eet in eet_ploeg])
     return kook_ploeg, eet_ploeg
 
+#####################################################
+# Uitgangspunt is dat al bepaald is welke eter bij welke koker komt.
+# En welke gang het betreft.
+# Hier wordt het adres van de koker toegevoegd aan de eter.
+# Afhankelijk van welke gang het betreft.
+
 
 def wijs_eter_aan_koker_toe(koker, eter1, gang):
     if gang == "voorgerecht":
@@ -28,30 +36,62 @@ def wijs_eter_aan_koker_toe(koker, eter1, gang):
         eter1.set_nagerecht(koker.get_adres())
     return koker, eter1
 
+#####################################################
+# Soms is het lastig een eter te plaatsen bij een koker,
+# met name als er al eters zijn toegewezen voor deze koker (en de betreffende gang).
+# Doorloop alle kokers
+# Check of de eter die nog niet is toegewezen, al eerder met deze koker heeft gegeten (bij een vorige gang).
+# door de lijst met eters te checken van de toe te wijzen eter.
+# Check dan
 
-def vind_een_koker(lijst_kokers, lijst_eters, eter_remie, gang):
+def vind_een_koker(lijst_kokers, lijst_eters, eter_toe_te_wijzen, gang):
+    print("[CHECK] vind een koker voor ", eter_toe_te_wijzen.adres)
+    lijst_van_eters_van_eter = eter_toe_te_wijzen.get_lijst_eters()
+    print("[CHECK] lijst met eters van eter is", lijst_van_eters_van_eter)
     geschikte_koker = ""
     gelukt = True
     for koker in lijst_kokers:
         koker_adres = koker.get_adres()
-        if koker_adres not in eter_remie.get_lijst_eters():
+        if koker_adres not in eter_toe_te_wijzen.get_lijst_eters():
             if gang == "voorgerecht":
+                print("[CHECK] voorgerecht, potentiele koker is", koker_adres)
                 lijst_voorgerecht = [veter for veter in lijst_eters if veter.voorgerecht == koker_adres]
-                if eter_remie.get_adres() not in lijst_voorgerecht:
+                print("[CHECK] voorgerecht_lijst", [h.adres for h in lijst_voorgerecht])
+                print("[CHECK], eter adres", eter_toe_te_wijzen.get_adres())
+                lijsten = [e.lijst_eters for e in lijst_voorgerecht]
+                alle_eters = [item for sublijst in lijsten for item in sublijst]
+                print("[CHECK] alle_eters", alle_eters)
+                if eter_toe_te_wijzen.get_adres() not in lijst_voorgerecht:
                     geschikte_koker = koker
+                    print("[CHECK] eter niet in lijst, succes")
                     break
             if gang == "hoofdgerecht":
+                print("[CHECK] hoofdgerecht, potentiele koker is", koker_adres)
                 lijst_hoofdgerecht = [heter for heter in lijst_eters if heter.hoofdgerecht == koker_adres]
-                if eter_remie.get_adres() not in lijst_hoofdgerecht:
+                print("[CHECK] lijst hoofdgerecht", [h.adres for h in lijst_hoofdgerecht])
+                print("[CHECK], eter adres", eter_toe_te_wijzen.get_adres())
+                lijsten = [e.lijst_eters for e in lijst_hoofdgerecht]
+                alle_eters = [item for sublijst in lijsten for item in sublijst]
+                print("[CHECK] alle_eters", alle_eters)
+                if eter_toe_te_wijzen.get_adres() not in lijst_hoofdgerecht:
                     geschikte_koker = koker
+                    print("[CHECK] eter niet in lijst, succes")
                     break
             if gang == "nagerecht":
+                print("[CHECK] nagerecht , potentiele koker is", koker_adres)
                 lijst_nagerecht = [neter for neter in lijst_eters if neter.nagerecht == koker_adres]
-                if eter_remie.get_adres() not in lijst_nagerecht:
+                print("[CHECK] lijst nagerecht", [h.adres for h in lijst_nagerecht])
+                print("[CHECK], eter adres", eter_toe_te_wijzen.get_adres())
+                lijsten = [e.lijst_eters for e in lijst_nagerecht]
+                alle_eters = [item for sublijst in lijsten for item in sublijst]
+                print("[CHECK] alle_eters", alle_eters)
+                if eter_toe_te_wijzen.get_adres() not in lijst_nagerecht:
                     geschikte_koker = koker
+                    print("[CHECK] eter niet in lijst, succes")
                     break
     if geschikte_koker == "":
         gelukt = False
+        print("[CHECK] alles is Mislukt")
     return geschikte_koker, gelukt
 
 
